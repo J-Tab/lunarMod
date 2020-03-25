2fa6c8776c663e69eb82ed12b37d00336da3b89f
Jeremy Tabujara <jemy_T20@hotmail.com>
2020-03-25 10:16:06 -0700

    Initial commit

---
+++ src/ofApp.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "ofMain.h"
+#include  "ofxAssimpModelLoader.h"
+#include "ParticleSystem.h"
+#include "ParticleEmitter.h"
+
+
+
+
+class ofApp : public ofBaseApp{
+
+	public:
+
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		void drawAxis(ofVec3f);
+		void initLightingAndMaterials();
+		void savePicture();
+		void toggleWireframeMode();
+		void togglePointsDisplay();
+		
+		
+		ofEasyCam cam;
+		ofxAssimpModelLoader lander;
+		ofLight light;
+		ofImage backgroundImage;
+		ofCamera *theCam = NULL;
+		ofCamera topCam;
+
+
+		bool bAltKeyDown;
+		bool bCtrlKeyDown;
+		bool bWireframe;
+		bool bDisplayPoints;
+	
+		bool bBackgroundLoaded = false;
+		bool bLanderLoaded = false;
+};
